6214eef48c315ca7d1296667e68013473b9ddd49
Алексей Зарубин <alken1t15@gmail.com>
2023-04-15 23:49:16 +0600

    version 3

--- sqll/query.sql
+++ sqll/query.sql
@@ -1,21 +1,33 @@
-CREATE TABLE product(
-    id SERIAL,
-    store_name VARCHAR(255),
-    category VARCHAR(255),
-    name VARCHAR(255),
+CREATE TABLE product
+(
+    id           SERIAL,
+    store_name   VARCHAR(255),
+    category     VARCHAR(255),
+    name         VARCHAR(255),
     link_product VARCHAR(255),
-    price INT4,
-    date TIMESTAMP
+    price        INT4,
+    date         TIMESTAMP
 );
-CREATE TABLE shop(
-    id SERIAL,
+CREATE TABLE shop
+(
+    id         SERIAL,
     store_name VARCHAR(255),
-    category VARCHAR(255),
-    link_page VARCHAR(255)
+    category   VARCHAR(255),
+    link_page  VARCHAR(255)
 );
 
-INSERT INTO shop (store_name, category, link_page) VALUES ('Белый ветер','Ноутбуки','https://shop.kz/noutbuki/filter/almaty-is-v_nalichii-or-ojidaem-or-dostavim/apply/');
-INSERT INTO shop (store_name, category, link_page) VALUES ('Белый ветер','Смартфоны','https://shop.kz/smartfony/filter/almaty-is-v_nalichii-or-ojidaem-or-dostavim/apply/');
-INSERT INTO shop (store_name, category, link_page) VALUES ('Белый ветер','Смарт-часы','https://shop.kz/smart-chasy/filter/almaty-is-v_nalichii-or-ojidaem-or-dostavim/apply/');
-INSERT INTO shop (store_name, category, link_page) VALUES ('Белый ветер','Планшеты','https://shop.kz/planshety/filter/almaty-is-v_nalichii-or-ojidaem-or-dostavim/apply/');
-INSERT INTO shop (store_name, category, link_page) VALUES ('Белый ветер','Телевизоры','https://shop.kz/televizory/filter/almaty-is-v_nalichii-or-ojidaem-or-dostavim/apply/');
+INSERT INTO shop (store_name, category, link_page)
+VALUES ('Р‘РµР»С‹Р№ РІРµС‚РµСЂ', 'РќРѕСѓС‚Р±СѓРєРё',
+        'https://shop.kz/noutbuki/filter/almaty-is-v_nalichii-or-ojidaem-or-dostavim/apply/');
+INSERT INTO shop (store_name, category, link_page)
+VALUES ('Р‘РµР»С‹Р№ РІРµС‚РµСЂ', 'РЎРјР°СЂС‚С„РѕРЅС‹',
+        'https://shop.kz/smartfony/filter/almaty-is-v_nalichii-or-ojidaem-or-dostavim/apply/');
+INSERT INTO shop (store_name, category, link_page)
+VALUES ('Р‘РµР»С‹Р№ РІРµС‚РµСЂ', 'РЎРјР°СЂС‚-С‡Р°СЃС‹',
+        'https://shop.kz/smart-chasy/filter/almaty-is-v_nalichii-or-ojidaem-or-dostavim/apply/');
+INSERT INTO shop (store_name, category, link_page)
+VALUES ('Р‘РµР»С‹Р№ РІРµС‚РµСЂ', 'РџР»Р°РЅС€РµС‚С‹',
+        'https://shop.kz/planshety/filter/almaty-is-v_nalichii-or-ojidaem-or-dostavim/apply/');
+INSERT INTO shop (store_name, category, link_page)
+VALUES ('Р‘РµР»С‹Р№ РІРµС‚РµСЂ', 'РўРµР»РµРІРёР·РѕСЂС‹',
+        'https://shop.kz/televizory/filter/almaty-is-v_nalichii-or-ojidaem-or-dostavim/apply/');
